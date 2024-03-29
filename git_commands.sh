765e18ed5c80e7e6ed9681fa27962867c7db685c
Nitin Kumar Singh <singh_nitin@iitgn.ac.in>
2024-03-29 16:25:57 +0530

    Git file sh

---
+++ git_commands.sh
@@ -0,0 +1,8 @@
+$ git config --global user.name "Your Name"
+$ git config --global user.email "your.email@example.com"
+$ git init = To create a new Git project
+$ git add file.txt = add files to git
+$ git status = check the status of git
+$ git commit -m "Your commit message here" = commit the changes with the git commit command
+$ git remote add origin https://github.com/username/repo.git
+$ git push -u origin master
